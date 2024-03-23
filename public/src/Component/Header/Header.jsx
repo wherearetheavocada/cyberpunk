4bdf3d4dd4ff9b1e41dc5c29d7057793bb71f597
Moneyflies <eremchuk.boris@gmail.com>
2024-03-23 16:12:32 +0300

    initialize files for Footer and Header, add icons for social media

---
+++ public/src/Component/Header/Header.jsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css'
+import logo from '/../../../img/Cyberpunk_2077_logo 1.jpg'
+
+export default function Header(){
+    return(
+        <div>
+            <div className={styles.Header}></div>
+        </div>
+    )
+}
